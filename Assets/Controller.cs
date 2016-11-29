82f94620bf2a7c614accab3d0ba309b09429e8bd
cuvette <franrizzo@hotmail.fr>
2016-11-29 12:19:49 +0100

    avancée vr
    on voit nos mains et on peut prendre des objets

---
+++ Assets/Controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+
+    public GameObject rightHand;
+    public GameObject leftHand;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandPos();
+        HandRot();
+
+    }
+
+    void HandPos() // Place the hand at the controller position
+    {
+        rightHand.transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        leftHand.transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+    }
+
+    void HandRot()
+    {
+        rightHand.transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+        leftHand.transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+    }
+
+}
---
+++ Assets/GrabObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabObject : MonoBehaviour
+{
+    private GameObject movable;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.One) && movable != null)
+        {
+            Debug.Log("Button A pushed");
+            movable.GetComponent<Rigidbody>().useGravity = false;
+            movable.gameObject.transform.position = this.transform.position;
+            movable.gameObject.transform.rotation = this.transform.rotation;
+        }
+
+        else if (OVRInput.Get(OVRInput.Button.One) == false && movable != null)
+        {
+            movable.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered a trigger");
+        movable = other.gameObject;
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        movable = null;
+    }
+}
+
